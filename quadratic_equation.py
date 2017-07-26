18992059c88512ba975e7bf2d65484f64a19deed
Evgeny Shestakov <evgeny.shestakov@gmail.com>
2017-07-26 11:12:14 +0000

    some fixes for Wally

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,11 +1,11 @@
 from math import sqrt
 
-def get_roots(a, b, c):
-    discriminant = b ** 2 - 4 * a * c
+def get_roots(a_coefficient, b_coefficient, c_coefficient):
+    discriminant = b_coefficient ** 2 - 4 * a_coefficient * c_coefficient
     if discriminant < 0:
         return None, None
-    root1 = (-b - sqrt(discriminant)) / (2 * a)
-    root2 = (-b + sqrt(discriminant)) / (2 * a)
+    root1 = (-b_coefficient - sqrt(discriminant)) / (2 * a_coefficient)
+    root2 = (-b_coefficient + sqrt(discriminant)) / (2 * a_coefficient)
     if discriminant == 0:
         return root1, None
     else:
